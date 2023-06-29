f1b726a80fc92abec4bb3827612ad8c643c5252c
unknown <brianna.albo@gmail.com>
2023-06-29 00:00:53 -0400

    adds cards

--- realtor-temp/src/App.js
+++ realtor-temp/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Row, Card } from "react-bootstrap";
+import Listings from './components/Listings';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Listings />
     </div>
   );
 }
